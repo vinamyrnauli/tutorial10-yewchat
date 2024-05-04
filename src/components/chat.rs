729c1bd8d9fbd5ef4fc5dc89f1bb7084997f01fc
vinamyrnauli <vinasllgn@gmail.com>
2024-05-05 00:34:22 +0700

    Websocket Phase 2

--- src/components/chat.rs
+++ src/components/chat.rs
@@ -3,6 +3,7 @@ use web_sys::HtmlInputElement;
 use yew::prelude::*;
 use yew_agent::{Bridge, Bridged};
 
+use crate::services::event_bus::EventBus;
 use crate::{services::websocket::WebsocketService, User};
 
 pub enum Msg {
@@ -43,6 +44,7 @@ pub struct Chat {
     chat_input: NodeRef,
     wss: WebsocketService,
     messages: Vec<MessageData>,
+    _producer: Box<dyn Bridge<EventBus>>,
 }
 impl Component for Chat {
     type Message = Msg;
@@ -75,6 +77,61 @@ impl Component for Chat {
             messages: vec![],
             chat_input: NodeRef::default(),
             wss,
+            _producer: EventBus::bridge(ctx.link().callback(Msg::HandleMsg)),
+        }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::HandleMsg(s) => {
+                let msg: WebSocketMessage = serde_json::from_str(&s).unwrap();
+                match msg.message_type {
+                    MsgTypes::Users => {
+                        let users_from_message = msg.data_array.unwrap_or_default();
+                        self.users = users_from_message
+                            .iter()
+                            .map(|u| UserProfile {
+                                name: u.into(),
+                                avatar: format!(
+                                    "https://avatars.dicebear.com/api/adventurer-neutral/{}.svg",
+                                    u
+                                )
+                                .into(),
+                            })
+                            .collect();
+                        return true;
+                    }
+                    MsgTypes::Message => {
+                        let message_data: MessageData =
+                            serde_json::from_str(&msg.data.unwrap()).unwrap();
+                        self.messages.push(message_data);
+                        return true;
+                    }
+                    _ => {
+                        return false;
+                    }
+                }
+            }
+            Msg::SubmitMessage => {
+                let input = self.chat_input.cast::<HtmlInputElement>();
+                if let Some(input) = input {
+                    let message = WebSocketMessage {
+                        message_type: MsgTypes::Message,
+                        data: Some(input.value()),
+                        data_array: None,
+                    };
+                    if let Err(e) = self
+                        .wss
+                        .tx
+                        .clone()
+                        .try_send(serde_json::to_string(&message).unwrap())
+                    {
+                        log::debug!("error sending to channel: {:?}", e);
+                    }
+                    input.set_value("");
+                };
+                false
+            }
         }
     }
 
--- src/services/websocket.rs
+++ src/services/websocket.rs
@@ -2,6 +2,9 @@ use futures::{channel::mpsc::Sender, SinkExt, StreamExt};
 use reqwasm::websocket::{futures::WebSocket, Message};
 
 use wasm_bindgen_futures::spawn_local;
+use yew_agent::Dispatched;
+
+use crate::services::event_bus::{EventBus, Request};
 
 pub struct WebsocketService {
     pub tx: Sender<String>,
@@ -15,6 +18,8 @@ impl WebsocketService {
 
         let (in_tx, mut in_rx) = futures::channel::mpsc::channel::<String>(1000);
 
+        let mut event_bus = EventBus::dispatcher();
+
         spawn_local(async move {
             while let Some(s) = in_rx.next().await {
                 log::debug!("got event from channel! {}", s);
@@ -27,11 +32,13 @@ impl WebsocketService {
                 match msg {
                     Ok(Message::Text(data)) => {
                         log::debug!("from websocket: {}", data);
+                        event_bus.send(Request::EventBusMsg(data));
                     }
                     Ok(Message::Bytes(b)) => {
                         let decoded = std::str::from_utf8(&b);
                         if let Ok(val) = decoded {
                             log::debug!("from websocket: {}", val);
+                            event_bus.send(Request::EventBusMsg(val.into()));
                         }
                     }
                     Err(e) => {
